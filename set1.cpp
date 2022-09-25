ca6007528aa0e905cd7ecae27f6796cc6e21f2bc
KSGit47 <kushagra4adrenaline@gmail.com>
2022-09-25 19:50:30 +0530

    Set 1 cpp sol.

---
+++ set1.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Sl{
+  public:
+  	int solve(vector<int> &ans){
+  	int k= accumulate(ans.begin(), ans.end(),0);
+  	int n = ans.size();
+  	int cur =0;
+  	pair<int,int> diff = {INT_MAX,0};
+    for(int i=0;i<n-1;i++){
+        cur += ans[i];
+        k-=ans[i];
+        int curr = abs((cur/(i+1))-(k/(n-i-1)));
+        if(curr < diff.first){
+        	diff ={curr, i};
+        }
+    }
+    return diff.second+1;
+  	}
+};
+int main(){
+	int n;
+	cin >> n ;
+	vector<int> ans(n);
+	for(int i=0;i<n;i++)
+		cin>>ans[i];
+	Sl ob;
+	cout << ob.solve(ans);
+	return 0;
+}
